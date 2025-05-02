70533fcce3f573ee37d1fee0e31869651046bafb
root <cooperg122@gmail.com>
2025-05-01 19:29:35 -0600

    snowflake connection almost working

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import pandas as pd
 from dotenv import load_dotenv
 import os
+from snowflake.connector import connect
+from snowflake.connector.pandas_tools import write_pandas
 
 def get_data_frame(url, params):
     response = requests.get(url, params=params)
@@ -15,33 +17,52 @@ def get_data_frame(url, params):
     else:
         print("Error, no data returned")
 
-def pull_data():
+def pull_data(starting_year = 2010, ending_year = 2022):
     load_dotenv()
-
     API_KEY = {
         "api_key": os.getenv("API_KEY"),  
     }
 
-    CSV_NAMES = ["carbon_emission_data.csv", "energy_data.csv"]
+    datasets = [
+        {"name": "carbon_emissions", "url": "https://api.eia.gov/v2/co2-emissions/co2-emissions-aggregates/data/?frequency=annual&data[0]=value&start=", "table": "CARBON_EMISSIONS"},
+        {"name": "energy", "url": "https://api.eia.gov/v2/total-energy/data/?frequency=annual&data[0]=value&start=", "table": "ENERGY"}
+    ]
 
-    STARTING_YEAR = 2010
-    ENDING_YEAR = 2022
-
-    for name in CSV_NAMES:
+    dataframes = []
+    for dataset in datasets:
         df = pd.DataFrame()
-        for year in range(STARTING_YEAR, ENDING_YEAR):
-            if name == CSV_NAMES[0]:
-                url = f"https://api.eia.gov/v2/co2-emissions/co2-emissions-aggregates/data/?frequency=annual&data[0]=value&start={year}&end={year + 1}&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
-            else:
-                url = f"https://api.eia.gov/v2/total-energy/data/?frequency=annual&data[0]=value&start={year}&end={year + 1}&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
-
+        for year in range(starting_year, ending_year):
+            url = f"{dataset["url"]}{year}&end={year + 1}&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
             this_year_df = get_data_frame(url, API_KEY)
             df = pd.concat([df, this_year_df], axis=0, ignore_index=True)
         print(df)
-        df.to_csv(name, index=False)
+        dataframes.append({"df": df, "table": dataset["table"]})
+
+    return dataframes
+
+def export_to_snowflake(df, table):
+    load_dotenv()
+
+    connection = connect(
+        user= os.getenv("SNOWFLAKE_USER"),
+        password= os.getenv("SNOWFLAKE_PASSWORD"),
+        account= os.getenv("SNOWFLAKE_ACCOUNT"),
+        warehouse='ENERGY_WH',
+        database='ENERGY_DB',
+        schema='RAW_DATA'
+    )
+
+    success, _, _, _ = write_pandas(connection, df, table)
+
+    if not success:
+        print(f"error exporting {table} data to Snowflake")
+
 
 def main():
-    pull_data()
+    dataframes = pull_data()
+
+    for dataframe in dataframes:
+        export_to_snowflake(dataframe["df"], dataframe["table"])
 
 if __name__ == "__main__":
     main()
